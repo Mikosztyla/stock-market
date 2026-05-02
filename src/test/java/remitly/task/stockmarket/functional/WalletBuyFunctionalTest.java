a2a10a8cab5cfd31140c203933fb95ba2a7ec77c
Mikosztyla <mikosztyla@gmail.com>
2026-05-03 00:02:23 +0200

    Sonar fixes

--- src/test/java/remitly/task/stockmarket/functional/WalletBuyFunctionalTest.java
+++ src/test/java/remitly/task/stockmarket/functional/WalletBuyFunctionalTest.java
@@ -3,7 +3,6 @@ package remitly.task.stockmarket.functional;
 import io.qameta.allure.Description;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import remitly.task.stockmarket.dto.WalletResponse;
 import remitly.task.stockmarket.steps.BankSteps;
 import remitly.task.stockmarket.steps.WalletSteps;
